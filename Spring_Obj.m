ff5b1b01be3b1057c7fe07e427297b7cecce4a0e
Tim Hodges <timothodge@gmail.com>
2015-07-14 23:14:09 -0400

    Added Spring Object

---
+++ Spring_Obj.m
@@ -0,0 +1,63 @@
+classdef Spring_Obj
+% This object is a container for all relevant parameters that a spring may
+% have. 
+%
+% Note: For end conditions 0 denotes closed ground,
+% 1 denotes open ground.
+%
+% Lengths and diameters are usually given in .0001 increments
+%
+%
+%
+%
+
+
+    properties(Access = private)
+        active_number_of_coils % N_a
+        pitch %p
+        solid_height % L_solid
+        diametral_expansion % d_expand
+    end
+    properties
+        wire_diameter %d_w
+        inner_diameter % d_i
+        outer_diameter % d_o
+        end_conditions % ec
+        force__at_open_position %F_open
+        shear_modulus % G
+        length_at_no_compression %L_free
+        length_at_open_position %L_open
+        length_at_close_position %L_close
+        length_at_hard_stop_position %L_hard
+        total_number_of_coils %N_t
+    end
+    
+ methods(Static)
+
+    end
+    
+    methods
+        function use_end_conditions(obj)
+            if obj.end_conditions == 0
+                %use close ground conditions
+                obj.active_number_of_coils = obj.total_number_of_coils - 2;
+                obj.pitch = (obj.length_at_no_compression - 2*obj.wire_diameter)/(obj.active_number_of_coils);
+                obj.solid_height = (obj.active_number_of_coils + 2)*obj.wire_diameter;
+                obj.diametral_expansion = sqrt((obj.inner_diameter + obj.wire_diameter)^2 + (obj.pitch^2 - obj.wire_diameter)/pi);
+            elseif obj.end_conditions == 1
+                %use open ground conditions
+                obj.active_number_of_coils = obj.total_number_of_coils - 1;
+                obj.pitch = (obj.length_at_no_compression)/(obj.active_number_of_coils + 1);
+                obj.solid_height = (obj.active_number_of_coils + 1)*obj.wire_diameter;
+               % obj.diametral_expansion 
+            else
+                sprintf('End conditions not specified.')
+            end
+                
+        end
+
+        
+    end
+    
+end
+
