f5214746dd5a7eaa3c2829d39b6555e4b9a26d08
Tianyu Qiu <newhardwarefound@gmail.com>
2015-07-19 17:36:36 -0400

    The new model for stress relaxation

--- Modules/Stress_Relaxation.m
+++ Modules/Stress_Relaxation.m
@@ -1,6 +1,7 @@
 function S = Stress_Relaxation( Spring )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+%Compute the stress relaxation when defelction, inner diameter, wire
+%diameter, shear modulus stress relaxation, Norton Bailey c,k,n, time
+%stress relaxation is available.
 
 formula = [-0.984183054718588   0.040484004765316
              -0.917598399222978   0.092121499837728
@@ -15,11 +16,12 @@ formula = [-0.984183054718588   0.040484004765316
               0.801578090733310   0.138873510219787
               0.917598399222978   0.092121499837728
               0.984183054718588   0.040484004765316];
+
 r=(formula(:,1)+1)/2*Spring.wire_diameter;
 w=formula(:,2)/2;
 
 theta = 2*Spring.deflection/pi/(Spring.total_number_of_coils-2)/(Spring.inner_diameter+Spring.wire_diameter)^2;
-Constant1 = 4/Spring.shear_modulus_stress_relaxation/theta/(Spring.wire_diameter/2)^4;
+Constant1 = 4/Spring.shear_modulus_stress_relaxation/theta/Spring.wire_diameter^4;
 Constant2 = ((Spring.Norton_Bailey_c/Spring.Norton_Bailey_k)*...
     Spring.shear_modulus_stress_relaxation*Spring.Norton_Bailey_n...
     *Spring.time_stress_relaxation^Spring.Norton_Bailey_k).^(-1/Spring.Norton_Bailey_n);
