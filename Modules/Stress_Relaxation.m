c20926219a570dbfbd2824418007cb6eaf5e4c47
Tianyu Qiu <newhardwarefound@gmail.com>
2015-07-19 17:12:13 -0400

    Stress Relaxation

---
+++ Modules/Stress_Relaxation.m
@@ -0,0 +1,31 @@
+function S = Stress_Relaxation( Spring )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+formula = [-0.984183054718588   0.040484004765316
+             -0.917598399222978   0.092121499837728
+             -0.801578090733310   0.138873510219787
+             -0.642349339440340   0.178145980761946
+             -0.448492751036446   0.207816047536889
+             -0.230458315955135   0.226283180262897
+              0.000000000000000   0.232551553230874
+              0.230458315955135   0.226283180262897
+              0.448492751036446   0.207816047536889
+              0.642349339440340   0.178145980761946
+              0.801578090733310   0.138873510219787
+              0.917598399222978   0.092121499837728
+              0.984183054718588   0.040484004765316];
+r=(formula(:,1)+1)/2*Spring.wire_diameter;
+w=formula(:,2)/2;
+
+theta = 2*Spring.deflection/pi/(Spring.total_number_of_coils-2)/(Spring.inner_diameter+Spring.wire_diameter)^2;
+Constant1 = 4/Spring.shear_modulus_stress_relaxation/theta/(Spring.wire_diameter/2)^4;
+Constant2 = ((Spring.Norton_Bailey_c/Spring.Norton_Bailey_k)*...
+    Spring.shear_modulus_stress_relaxation*Spring.Norton_Bailey_n...
+    *Spring.time_stress_relaxation^Spring.Norton_Bailey_k).^(-1/Spring.Norton_Bailey_n);
+
+S = sum(Constant1*(w.*r.^2.*((Spring.shear_modulus_stress_relaxation*theta*r).^(-Spring.Norton_Bailey_n)+Constant2).^(-1/Spring.Norton_Bailey_n)));
+
+
+end
+
